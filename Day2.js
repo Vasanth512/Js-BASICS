a3b744e91d82ae221cb499e630336ec8d80beea5
Vasanth Reddy <vasanthreddy493@gmail.com>
2021-11-18 15:03:58 +0530

    day2

---
+++ Day2.js
@@ -0,0 +1,33 @@
+// Problem 1
+function findFreq(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) == char) {
+            count += 1;
+        }
+    }
+    return count;
+}
+console.log(findFreq('How many times does the character occur in this sentence?', 'm'));
+console.log(findFreq('How many times does the character occur in this sentence?', 'h'));
+
+//Problem-2
+function areAllEqual(arr) {
+    var first = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (first !== arr[i]) {
+            return false;
+        }
+
+        return true;
+    }
+}
+console.log(areAllEqual([1, 2, 1, 1]));
+console.log(areAllEqual(['test', 'test', 'test']));
+//Problem 3
+function subArray(arr, n) {
+    const newarray = arr.splice(-n);
+    return newarray;
+}
+console.log(subArray([1, 2, 3, 4, 5], 2));
+console.log(subArray([1, 2, 3], 6));
